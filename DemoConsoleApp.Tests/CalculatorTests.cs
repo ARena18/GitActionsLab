545bedbf3ed95327cc113aafe1b68dc396b86a01
ahnrena.18@gmail.com <ahnrena.18@gmail.com>
2026-05-19 20:01:48 -0700

    initial commit with app, tests, and CI workflow

---
+++ DemoConsoleApp.Tests/CalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using DemoConsoleApp;
+using Xunit;
+
+namespace DemoConsoleApp.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsCorrectValue()
+    {
+        int result = Calculator.Add(2, 3);
+        Assert.Equal(5, result);
+    }
+}
