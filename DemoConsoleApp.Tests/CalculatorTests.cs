3e3d48ac75184998a2c96427af8d0fc299e130b6
ahnrena.18@gmail.com <ahnrena.18@gmail.com>
2026-05-19 20:31:21 -0700

    add neg value test

--- DemoConsoleApp.Tests/CalculatorTests.cs
+++ DemoConsoleApp.Tests/CalculatorTests.cs
@@ -8,6 +8,13 @@ public class CalculatorTests
     [Fact]
     public void Add_ReturnsCorrectValue()
     {
+        int result = Calculator.Add(2, 3);
+        Assert.Equal(5, result);
+    }
+
+    [Fact]
+    public void Add_NegValue_ReturnsCorrectValue()
+    {
         int result = Calculator.Add(-2, 3);
         Assert.Equal(1, result);
     }
